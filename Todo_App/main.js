70af54a3edea2dd938e7cd51be63c577f4bf35bb
ubaid Dev <obaidkhanzada2004@gmail.com>
2024-04-08 04:22:12 +0500

    add update feature and publish on npm

--- Todo_App/main.js
+++ Todo_App/main.js
@@ -1,27 +1,32 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
 import PromptSync from "prompt-sync";
 const prompt = PromptSync();
-let mylist = ["ubaid", "khan"];
+let mylist = [];
 console.log("\n===============================Welcome To My Todo_App======================================\n\n");
 while (true) {
     let Task = await inquirer.prompt([
         {
             name: "querry",
-            message: "Choose What you do with this task (use arrow key) ",
+            message: "Select what action you'd like to take with this task. (use arrow key) ",
             type: "list",
-            choices: ["read", "add_Todo", "Delete_todo", "Quit"]
-        }
+            choices: ["Read", "Add Todo", "Update", "Delete Todo", "Quit"],
+        },
     ]);
     let saveTask = Task.querry;
-    if (saveTask === "read") {
-        console.log("================================View Our Todo_List======================================");
-        //  console.log(...mylist)
-        for (let item of mylist) {
-            console.log(mylist.indexOf(item) + ":" + item);
+    if (saveTask === "Read") {
+        if (mylist[0] === undefined) {
+            console.log("\n<<<<<<<<<<<<<<<<<<<<-----No List Found------>>>>>>>>>>>>>>>>>>>\n");
         }
-        console.log("========================================================================================");
+        else {
+            console.log("\n================================View Our Todo_List======================================\n");
+            for (let item of mylist) {
+                console.log(mylist.indexOf(item) + ":" + item);
+            }
+            console.log("\n\n========================================================================================\n");
+        }
     }
-    else if (saveTask === "add_Todo") {
+    else if (saveTask === "Add Todo") {
         let input = prompt("Enter your todo do you want to add :");
         if (input === "") {
             console.log("   please add valid todo  \n");
@@ -29,19 +34,37 @@ while (true) {
         else {
             mylist.push(input);
             console.log("\n\t\tSuccessfully add todo \n");
-            // console.log("   \n            here is the list   \n     ")
-            // console.log(mylist)
         }
     }
-    else if (saveTask === "Delete_todo") {
-        console.log("\n \t Here is the list.\n");
-        console.log(mylist);
-        let delInput = prompt("\nType the index of the todo you want to delete :");
-        console.log("\n --------------Remove todo---------------\n");
-        mylist.splice(delInput, 1);
+    else if (saveTask === "Delete Todo") {
+        if (mylist[0] === undefined) {
+            console.log("\n<<<<<<<<<<<<<<<<<<<<-----No List Found------>>>>>>>>>>>>>>>>>>>\n");
+        }
+        else {
+            console.log("\n \t Here is the list.\n");
+            console.log(mylist);
+            let delInput = prompt("\nType the index of the todo you want to delete :");
+            console.log("\n --------------Remove todo---------------\n");
+            mylist.splice(delInput, 1);
+        }
+    }
+    else if (saveTask === "Update") {
+        if (mylist[0] === undefined) {
+            console.log("\n<<<<<<<<<<<<<<<<<<<<-----No List Found------>>>>>>>>>>>>>>>>>>>\n");
+        }
+        else {
+            console.log("==========================here is the list===========================");
+            let updateInput = prompt("Tpye index of list do you to update it: ");
+            mylist.splice(updateInput, 1);
+            let updateTodo = prompt("Enter your update todo ");
+            mylist.push(updateTodo);
+            console.log("\t\t Update Successfully\t\t");
+            console.log("\n\nHere is the Updated List\n");
+            console.log(mylist);
+        }
     }
     else if (saveTask === "Quit") {
-        console.log("<<<<<<<<<<<<<<<<<<<<<-----------Thank's for using my todo_App---------->>>>>>>>>>>>>>>>>>>>>>");
+        console.log("\n<<<<<<<<<<<<<<<<<<<<<-----------Thank's for using my todo_App---------->>>>>>>>>>>>>>>>>>>>>>");
         break;
     }
 }
