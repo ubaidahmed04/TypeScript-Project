19aede49221e2a20e3bf7210968858c3dc8ba674
ubaid Dev <obaidkhanzada2004@gmail.com>
2024-04-08 03:28:31 +0500

    add read delete feature

--- Todo_App/index.js
+++ Todo_App/index.js
@@ -1,5 +1,24 @@
+import inquirer from "inquirer";
 let todo = [];
-let value = "name";
-value = todo.push();
-console.log(value);
-export {};
+while (true) {
+    let userTodo = await inquirer.prompt([
+        {
+            name: "todoList",
+            message: "Enter your todo  or (type 'exit' to stop!)",
+            type: "string",
+        },
+    ]);
+    let saveTodo = userTodo.todoList.trim();
+    if (saveTodo === "") {
+        console.log("please add your todo");
+    }
+    if (saveTodo.toLowerCase() === "exit") {
+        break;
+    }
+    else {
+        todo.push(saveTodo);
+        // console.log(saveTodo)
+        console.log("your todo is ", todo);
+    }
+}
+console.log("your final todo is ", todo);
---
+++ Todo_App/main.ts
@@ -0,0 +1,54 @@
+ import inquirer from "inquirer";
+import PromptSync from "prompt-sync";
+const prompt =PromptSync();
+ let mylist:any = [];
+console.log("\n===============================Welcome To My Todo_App======================================\n\n");
+while(true){
+ let Task = await inquirer.prompt(
+    [
+        {
+            name :"querry",
+            message:"Choose What you do with this task (use arrow key) ",
+            type:"list",
+            choices:["read","add_Todo","Delete_todo","Quit"]
+        }
+    ]
+ )
+  let saveTask = Task.querry;
+ if(saveTask === "read"){
+     
+     console.log("================================View Our Todo_List======================================");
+    //  console.log(...mylist)
+    for(let item of mylist){
+     console.log(mylist.indexOf(item)+":"+item)
+
+    }
+     console.log("========================================================================================");
+ }
+ else if(saveTask === "add_Todo"){
+    let input = prompt("Enter your todo do you want to add :");
+    if(input ===""){
+        console.log("   please add valid todo  \n")
+    }  
+    else{
+
+        mylist.push(input);
+        console.log("\n\t\tSuccessfully add todo \n")
+        // console.log("   \n            here is the list   \n     ")
+        // console.log(mylist)
+    }  
+ }
+ else if(saveTask === "Delete_todo"){
+    console.log("\n \t Here is the list.\n");
+    console.log(mylist);
+    let delInput = prompt("\nType the index of the todo you want to delete :")
+    console.log("\n --------------Remove todo---------------\n")
+    mylist.splice(delInput,1)    
+
+ }
+ else if(saveTask === "Quit"){
+    console.log("<<<<<<<<<<<<<<<<<<<<<-----------Thank's for using my todo_App---------->>>>>>>>>>>>>>>>>>>>>>")
+    break;
+ }
+}
+ 
